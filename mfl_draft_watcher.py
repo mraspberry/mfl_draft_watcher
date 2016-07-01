d3ae26ae457e717a4dbabfed0d42cece46fac15d
Matthew Raspberry <nixalot@nixalot.com>
2016-07-01 19:24:03 +0000

    Fixed multiple issues in mfl_draft_watcher.py. Listed below:
    Fixed issue with write-in players drafted that aren't in MFL DB
    Switched to OrderedDict's where appropriate to preserve order from MFL

--- mfl_draft_watcher.py
+++ mfl_draft_watcher.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 
+import collections
 import fasteners
 import json
 import logging
@@ -19,6 +20,7 @@ _LOG = os.path.join(_LOGDIR,'draft_watcher.log')
 _PLAYERS = os.path.join(_CACHEDIR,'players.json')
 _FRANCHISES = os.path.join(_CACHEDIR,'franchises.json')
 _DRAFTRES = os.path.join(_CACHEDIR,'draftResults.json')
+_FULL_LEAGUE = os.path.join(_CACHEDIR,'leagueinfo.json')
 _MFL_ENDPOINT = 'http://football.myfantasyleague.com/{}/export'.format(date.today().year)
 
 def make_dirs(dirname):
@@ -70,6 +72,11 @@ def get_players():
     
     return players
 
+def cache_league_res(results):
+    logging.debug('Caching full league results')
+    with open(_FULL_LEAGUE,'w',encoding='utf8') as league_fh:
+        json.dump(results,league_fh)
+
 def fetch_teams(leagueid):
     params = {
         'JSON': 1,
@@ -77,17 +84,19 @@ def fetch_teams(leagueid):
         'L': leagueid,
     }
     results = requests.get(_MFL_ENDPOINT,params=params)
-    franchises = results.json()['league']['franchises']['franchise']
+    json_res = results.json()
+    cache_league_res(json_res)
+    franchises = json_res['league']['franchises']['franchise']
     return index_by_id(franchises)
 
 def get_teams(leagueid):
     if old(_FRANCHISES):
         logging.debug('Updating franchise cache')
         teams = fetch_teams(leagueid)
-        with open(_FRANCHISES,'w') as franchise_fh:
+        with open(_FRANCHISES,'w',encoding='utf8') as franchise_fh:
             json.dump(teams,franchise_fh)
     else:
-        with open(_FRANCHISES,'r') as franchise_fh:
+        with open(_FRANCHISES,'r',encoding='utf8') as franchise_fh:
             teams = json.load(franchise_fh)
     return teams
 
@@ -98,13 +107,19 @@ def post_to_gm(msg):
     logging.debug('Result of GM post: %i',result.status_code)
 
 def load_prev_draft_info():
-    with open(_DRAFTRES,'r') as draft_fh:
+    with open(_DRAFTRES,'r',encoding='utf8') as draft_fh:
         draft_info = json.load(draft_fh)
     return draft_info
 
 def write_draft_info(info):
-    with open(_DRAFTRES,'w') as draft_fh:
+    with open(_DRAFTRES,'w',encoding='utf8') as draft_fh:
         json.dump(info,draft_fh)
+
+def get_league_name():
+    with open(_FULL_LEAGUE,encoding='utf8') as league_fh:
+        results = json.load(league_fh)
+
+    return results['league']['name']
 
 def get_draft_info(leagueid):
     params = {
@@ -115,21 +130,23 @@ def get_draft_info(leagueid):
     try:
         prev_info = load_prev_draft_info()
     except IOError:
-        prev_info = dict()
+        prev_info = collections.OrderedDict()
 
     results = requests.get(_MFL_ENDPOINT,params=params)
-    new_info = dict()
-    getter = operator.itemgetter('franchise','round','pick')
+    new_info = collections.OrderedDict()
+    getter = operator.itemgetter('franchise','round','pick','player')
     picks = results.json()['draftResults']['draftUnit']['draftPick']
 
     # if x['timestamp'] is an empty string the pick hasn't been made yet
     for pick in filter(lambda x: x['timestamp'],picks):
         prevkey = '_'.join(getter(pick))
         if not prevkey in prev_info:
+            logging.debug('%s not a key in prev_info',prevkey)
             new_info[prevkey] = [pick]
 
     # add in our new data
     prev_info.update(new_info)
+    write_draft_info(prev_info)
 
     return new_info
 
@@ -141,7 +158,7 @@ def main():
     leagueid = 52269
     logger = logging.getLogger(None)
     handle = logging.handlers.TimedRotatingFileHandler(_LOG,when='midnight',backupCount=7)
-    formatter = logging.Formatter('%(asctime)s | %(levelname)s | %(message)s')
+    formatter = logging.Formatter('%(asctime)s|%(levelname)8s|%(message)s')
     handle.formatter = formatter
     logger.addHandler(handle)
     logger.setLevel(logging.DEBUG)
@@ -150,35 +167,50 @@ def main():
         players = get_players()
         logging.info('Getting teams')
         teams = get_teams(leagueid)
+        leaguename = get_league_name()
         draft_info = get_draft_info(leagueid)
         if not draft_info:
             logging.info('No new picks made. Exiting')
-            sys.exit()
+            # return here so we do the lock cleanup
+            return
         msglist = list()
-        msg_template = 'With the {num} pick in the To Pimp a Dynasty Draft, {team} selects {player} {pos}'
+        ##template = 'With the number {num} pick in the {ln} draft, {team} selects {player} {pos}'
+        template = '{num}: {player}, {pos}, {team}'
         for draftkey,draftval in draft_info.items():
             # calculate actual pick number instead of round <num> pick <num>
-            roundbase = 12 * (int(draftval['round']) - 1)
-            picknum = roundbase + int(draftval['pick'])
+            draftval = draftval[0]
+            ##roundbase = 12 * (int(draftval['round']) - 1)
+            ##picknum = roundbase + int(draftval['pick'])
+            roundbase = int(draftval['round'])
+            pick = draftval['pick']
+            picknum = '{}.{}'.format(roundbase,pick)
             logging.debug(pprint.pformat(draftval))
-            playerinfo = players[draftval['player']]
+            try:
+                playerinfo = players[draftval['player']]
+            except KeyError:
+                playerinfo = { 'name': 'Devy Draft Pick', 'position': 'N/A' }
             name = playerinfo['name']
             teaminfo = teams[draftval['franchise']]
             team = teaminfo['name']
             position = playerinfo['position']
-            msg = msg.format(
+            msg = template.format(
+                ##ln=leaguename,
                 num=picknum,
-                team=team,
                 player=name,
                 pos=position,
+                team=team,
             )
             msglist.append(msg)
+            logging.debug('Added "%s" to msglist',msg)
     except Exception:
         logging.exception('Caught unhandled exception')
+        raise
 
     if msglist:
         message = '\n'.join(msglist)
+        logging.debug('GM Message: "%s"',repr(message))
         post_to_gm(message)
 
 if __name__ == '__main__':
     main()
+    sys.exit()
